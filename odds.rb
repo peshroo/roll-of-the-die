6c450882601651638b2d1471235b31e130f27b34
Roopesh <rrattan.89@gmail.com>
2017-05-18 18:01:20 -0400

    Odds

--- odds.rb
+++ odds.rb
@@ -2,22 +2,16 @@ permutations = {}
 
 (2..12).each {|number| permutations[number] = 0}
 
-count = 0
-
 (1..6).each do |roll1|
   (1..6).each do |roll2|
-    puts "Dice Roll: #{roll1}, #{roll2}"
-    puts "Total: #{roll1 + roll2}"
-    count += 1
     permutations[roll1 + roll2] += 1
   end
 end
 
 permutations.each { |key, value| puts "#{key} occurs #{value} times" }
 
-number_of_permutations = 0
 
 permutations.each do |key, value|
-  odds = value.to_f / number_of_permutations * 100
-  puts "The odds of #{key} coming up are #{odds.round}%"
+  odds = value/36.0 * 100.0
+  puts "The odds of #{key} coming up are #{odds}%"
 end
